84132a071f6258a9a5622ce2b08ac841ab90b71f
Eric Chang <efrochang@gmail.com>
2018-06-03 11:34:34 -0500

    06 Fake Service call 1

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,35 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+
 describe('TodosComponent', () => {
+
   let component: TodosComponent;
 
-  beforeEach(() => {
+  let todoService: TodoService;
+
+  beforeEach(() => 
+  {
+    todoService = new TodoService( null );                  // Cheat and pass in null for Http
+    component = new TodosComponent( todoService );
   });
 
-  it('', () => {
+  it('should set the todos property when the items returned from the HTTP server', () => {
+
+    // Arrange
+    let todosStub = [ 1, 2, 3 ];                            // The fake result array
+    spyOn( todoService, 'getTodos').and.callFake( () => {   // [KEY]: Use "spyOn()" to mock the Service method call
+
+      return Observable.from( [ todosStub ] );              // [KEY]: Fake returning Observable with an array
+    });
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    expect( component.todos ).toBe( todosStub );
+
   });
 });
